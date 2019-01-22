3069fe3ac034efa02dcf4dd5b4e694b9431dfd42
Danny Blum <danny.blum@gmail.com>
2019-01-21 18:55:40 -0800

    Map each whole string instead of individual chars

--- problems/group_anagrams.cpp
+++ problems/group_anagrams.cpp
@@ -1,42 +1,23 @@
 class Solution {
 public:
     
-    bool mapEqual(map<char, int> &m1, map<char, int> &m2) {
-        if (m1.size() != m2.size()) return false;
-        
-        return std::equal(m1.begin(), m1.end(), m2.begin());
-    }
-    
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> vvout;
-        vector<map<char, int>> vmana;
+        unordered_map<string, vector<string>> mana;
         
         for(int i=0; i<strs.size(); ++i)
         {
             string s = strs[i];
-            map<char, int> m;
-            for (int j=0; j<s.length(); ++j)
-            {
-                if (m.find(s[j]) == m.end())
-                    m[s[j]] = 1;
-                else
-                    m[s[j]]++;
-            }
-            bool found = false;
-            for (int ana=0; ana<vvout.size(); ++ana)
-            {
-                if (mapEqual(m, vmana[ana]))
-                {
-                    found = true;
-                    vvout[ana].push_back(s);
-                    break;
-                }
-            }
-            if (!found)
-            {
-                vvout.push_back(vector<string>(1,s));
-                vmana.push_back(m);
-            }                  
+            string ssort = s;
+            std::sort(ssort.begin(), ssort.end());
+            mana[ssort].push_back(s);
+        }
+         
+        unordered_map<string, vector<string>>::iterator mana_iter;
+        for (mana_iter=mana.begin(); mana_iter!=mana.end(); ++mana_iter)
+        {
+            vector<string> v(mana_iter->second.begin(), mana_iter->second.end());
+            vvout.push_back(v);   
         }
 
         return vvout;
